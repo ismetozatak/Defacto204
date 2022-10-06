5a54c3d3f2d7a5889d2868fbda379779c3acec26
Ismet Ozatak <ismet.ozatak@defacto.com>
2022-10-06 13:54:52 +0300

    first commit

---
+++ IaaS/VM/createVmWithCli.sh
@@ -0,0 +1,7 @@
+az login powershell üzerinden
+# Azuredan oluşturuluan hesap girilir.
+az account set --subscription "Azure Pass - Sponsorluk"   #"00d099f1-d183-48d2-a3d3-b319d4816d28"
+
+#1. resource group listeleme 
+az group list --output table
+
